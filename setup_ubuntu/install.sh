018d9d2957a9d5fd6ea40afaacf8a47ad5045d7a
Chaudier Christophe <chaudier.christophe@gmail.com>
2014-02-08 20:45:01 +0100

    install package ok

--- setup_ubuntu/install.sh
+++ setup_ubuntu/install.sh
@@ -14,22 +14,25 @@ trace() {
 }
 
 run() {
-    echo "cmd : $*"
+    echo "==[cmd : $*i]=="
     $* || sortie 1
 }
 
 test_os() {
     trace "Test OS version"
     grep "$os_version" /etc/issue >/dev/null|| (trace "OS not $os_version" && sortie 1)
-    trace "Os ok"
+    trace " --> Os OK"
 
 }
 
 install_packages(){
     trace "Installation packages"
-    run echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
-    run apt-get update && apt-get upgrade -y && apt-get dist-upgrade
-    run apt-get install -y zsh curl git-core
+    run sudo 'echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list'
+    run sudo apt-get update 
+    run sudo apt-get upgrade -y 
+    run sudo apt-get dist-upgrade
+    run sudo apt-get install -y zsh curl git-core
+    trace " --> packages OK"
 }
 
 create_users() {
@@ -41,11 +44,10 @@ create_users() {
 create_user() {
     user=$1
     trace "Create user : $1user"
-    id $user || run adduser $user --disabled-password
+    id $user || run sudo adduser $user --disabled-password
 }
 
 trace "Installation du minimal vital sur $os_version serveur"
-
 test_os
 install_packages
 create_users
